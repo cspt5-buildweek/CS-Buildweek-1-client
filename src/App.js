b6cc11dbc7313fa58c41e4b80758d99871bea35d
Tim Dill <brodt7258@gmail.com>
2020-02-27 06:38:48 -0500

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import UserContext from './store/UserContext';
+import GamePage from './pages/GamePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const [userData, setUserData] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{userData, setUserData}}>
+      <BrowserRouter>
+        <Switch>
+
+          <Route exact path="/">
+            <GamePage />
+          </Route>
+          
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
