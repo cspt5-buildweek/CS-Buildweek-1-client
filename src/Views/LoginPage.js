8c1313d19543fef38256ad8300918d435e0da244
Brandon Vilaychith <blvilaychith@gmail.com>
2020-02-27 17:46:34 -0600

    initial login setup

--- src/Views/LoginPage.js
+++ src/Views/LoginPage.js
@@ -1,14 +1,38 @@
-import React from "react";
-import { useInput } from "../CustomHooks/InputHook"
+import React, { useState } from "react";
+import axios from "axios"
+// import { useInput } from "../CustomHooks/InputHook"
 
 function LoginPage() {
-  const { userValue, userBind, userReset } = useInput("")
-  const { passValue, passBind, passReset } = useInput("")
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: ""
+  })
+  // Handles input changes to user info for login
+  const handleChange = e => {
+    setUserInfo({
+      ...userInfo,
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
+  // Handles submission to backend for login 
+  // IMPORTANT set up a single variable for base URL in .env 
+  // TODO Redirection to home game page once key is received
+  // TODO Indicator of waiting for login from server
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios.post("https://lambda-mud-test.herokuapp.com/api/login/", userInfo)
+    .then(response => {
+      console.log(response)
+    })
+    // TODO Must set up a .catch for failure 
+  }
+  
   return (
     <div>
       <h1>LOGIN PAGE</h1>
-      <input type="text" {...userBind}/>
-      <input type="text" {...passBind}/>
+      <input type="text" name="username" onChange={handleChange}/>
+      <input type="text" name="password" onChange={handleChange}/>
+      <button onClick={handleSubmit} type="submit">Submit</button>
     </div>
   );
 }
