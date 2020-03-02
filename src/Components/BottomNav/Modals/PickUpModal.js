efef7059bf6847d6426f203be54fdbd50b5a140f
Brandon Vilaychith <brandonlvilaychith@gmail.com>
2020-03-01 21:13:21 -0600

    modals set up

---
+++ src/Components/BottomNav/Modals/PickUpModal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PickUpModal() {
+    return (
+        <h1>Pick Up</h1>
+    )
+}
+
+export default PickUpModal;
---
+++ src/Components/BottomNav/Modals/ShopModal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ShopModal() {
+    return (
+        <h1>Shop</h1>
+    )
+}
+
+export default ShopModal;
