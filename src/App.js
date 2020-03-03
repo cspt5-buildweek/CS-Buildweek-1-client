4f538b898cb9181d0d5697c6f77a8ea3a1f85aea
Tim Dill <brodt7258@gmail.com>
2020-03-03 00:06:59 -0500

    fix imports for merge

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import useUserContext from './hooks/useUserContext';
 
 // Components
-import InfoBar from "./Components/BottomNav/InfoBar";
+import InfoBar from "./components/BottomNav/InfoBar";
 import GamePage from './pages/GamePage';
 import LoginPage from './pages/LoginPage';
 
